cf960f776d53af1e8a75425c16001274c9e55203
cledisonxs <155705271+cledisonxs@users.noreply.github.com>
2024-12-11 01:53:02 -0300

    UI

--- algoritmo-js/ex012/ex012.js
+++ algoritmo-js/ex012/ex012.js
@@ -1,5 +1,16 @@
 function calcular(){
-    
-    var precoDesconto = preco * 0.005;
+    var txtp = document.getElementById('precoProduto');
+    var txtd = document.getElementById('desconto');
+    var resultado =  document.getElementById('res');
+
+    var preco = Number(txtp.value)
+    var desconto =  Number(txtd.value)
+    var precoPromocional =  preco - (preco * desconto/100);
+
+    resultado.innerHTML = `O produto <strong>sem desconto </strong> fica <strong>R$${preco.toFixed(2)}</strong>`
+
+    resultado.innerHTML += `<p><strong>O valor do desconto é de ${desconto}%.</strong></p>`
+
+    resultado.innerHTML += `<p>O produto <strong>com desconto</strong> fica <strong>R$${precoPromocional.toFixed(2)}</strong></p>`
     ;
 }
