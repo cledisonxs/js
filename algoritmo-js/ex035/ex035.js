d4338aa8064041dee0d27c67abf42fed1664092d
cledisonxs <155705271+cledisonxs@users.noreply.github.com>
2025-01-07 02:24:59 -0300

    atualizacao 07/01/25

--- algoritmo-js/ex035/ex035.js
+++ algoritmo-js/ex035/ex035.js
@@ -27,7 +27,7 @@ function calcular(){
             res.innerHTML += ` <p> Você alugou o veículo  por ${dias_alugado} dia(s) e percorreu com ele ${km_percorridos}km.Você irá pagar pelo aluguel do carro R$ ${preco_dias_alugado.toFixed(2)}, e pelos quilômetros percorridos R$ ${preco_km_percorridos.toFixed(2)}.Somando os dois, você irá pagar R$ ${preco_total.toFixed(2)}</p>`;
 
         }else if(km_percorridos > 100) {
-            var preco_km_percorridos = km_percorridos * 0.10;
+            var preco_km_percorridos = 100 * 0.20 + (km_percorridos - 100) * 0.10;
             var preco_total = preco_dias_alugado + preco_km_percorridos;
 
             res.innerHTML += ` <p> Você alugou o veículo  por ${dias_alugado} dia(s) e percorreu com ele ${km_percorridos}km.Você irá pagar pelo aluguel do carro R$ ${preco_dias_alugado.toFixed(2)}, e pelos quilômetros percorridos R$ ${preco_km_percorridos.toFixed(2)}.Somando os dois, você irá pagar R$ ${preco_total.toFixed(2)}</p>`;
@@ -46,7 +46,7 @@ function calcular(){
             res.innerHTML += ` <p> Você alugou o veículo  por ${dias_alugado} dia(s) e percorreu com ele ${km_percorridos}km.Você irá pagar pelo aluguel do carro R$ ${preco_dias_alugado.toFixed(2)}, e pelos quilômetros percorridos R$ ${preco_km_percorridos.toFixed(2)}.Somando os dois, você irá pagar R$ ${preco_total.toFixed(2)}</p>`;
 
         }else if(km_percorridos > 200){
-            var preco_km_percorridos = km_percorridos * 0.25;
+            var preco_km_percorridos = 200 * 0.30 + (km_percorridos - 200) * 0.25;
             var preco_total = preco_dias_alugado + preco_km_percorridos;
 
             res.innerHTML += ` <p> Você alugou o veículo  por ${dias_alugado} dia(s) e percorreu com ele ${km_percorridos}km.Você irá pagar pelo aluguel do carro R$ ${preco_dias_alugado.toFixed(2)}, e pelos quilômetros percorridos R$ ${preco_km_percorridos.toFixed(2)}.Somando os dois, você irá pagar R$ ${preco_total.toFixed(2)}</p>`;
