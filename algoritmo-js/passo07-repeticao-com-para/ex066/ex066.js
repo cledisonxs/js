cf3ed6e7e4d21e942e153dde09e41ef4babc5128
cledisonxs <155705271+cledisonxs@users.noreply.github.com>
2025-01-29 02:39:50 -0300

    atualizacao - 29/01/25

---
+++ algoritmo-js/passo07-repeticao-com-para/ex066/ex066.js
@@ -0,0 +1,14 @@
+function tabuada() {
+    let tab = document.getElementById('seltab');
+    const num = document.getElementById('num');
+
+    tab.innerHTML = ``;
+    for(let c = 0; c <= 10; c++){
+        let n = Number(num.value);
+        let item = document.createElement('option');
+        item.text = `${n} x ${c} = ${n*c}`;
+        item.value = `tab${c}`//!serve para facilitar o banco de dados
+        tab.appendChild(item);
+    
+    }
+}
